e196820862f0b55805e84c75c922dc4360e0d27e
gabreisndev <gabrielsn300@gmail.com>
2024-08-08 22:58:12 -0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -14,15 +15,19 @@ logging.basicConfig(level=logging.INFO, filename='app.log', filemode='a', format
 class Main:
 
     def start(self):
-#        self.options = uc.ChromeOptions()
+        self.options = uc.ChromeOptions()
         self.options.add_argument("--start-maximized")
         self.options.add_argument("--disable-infobars")
         self.options.add_argument("--disable-extensions")
         self.options.add_argument("--no-sandbox")
         self.options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-#        self.driver = uc.Chrome(options=self.options)
-        self.driver = webdriver.Chrome()
+        self.driver = uc.Chrome(options=self.options)
+
         self.driver.get("https://superbet.com/pt-br/intro")
+        sleep(7)
+        self.driver.find_element(By.CSS_SELECTOR, "button.sds-button.sds-focus.e2e-login.sds-button--md sds-button--primary-color").click()
+        sleep(4)
+        self.driver.quit()
         sleep(100)
 
                    
