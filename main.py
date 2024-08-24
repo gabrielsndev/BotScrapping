861a5b50336a599ad91207d03d4a845e87563216
gabreisndev <gabrielsn300@gmail.com>
2024-08-23 21:47:06 -0300

    Reajustando código

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 import subprocess
+import platform
 import logging
 import json
 import os
@@ -16,23 +18,35 @@ logging.basicConfig(level=logging.INFO, filename='app.log', filemode='a', format
 class Main:
         
     def start(self):
+        system = platform.system()
+        
+        if system == "Windows":
+            subprocess.Popen(
+                f'"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" --log-level=3 --remote-debugging-port=9222 --disable-popup-blocking', shell=True)
+        elif system == "Linux":
+            subprocess.Popen(
+                f'/usr/bin/google-chrome --log-level=3 --remote-debugging-port=9222', shell=True)
+        
+        sleep(1)
+        service = Service()
+        self.options = webdriver.ChromeOptions()
 
-        os.system('taskkill /IM chrome.exe /F')
-        subprocess.Popen(
-       '"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" --log-level=3 --remote-debugging-port=9222', shell=True)
-        self.options = uc.ChromeOptions()
         self.options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
         self.options.add_argument("--start-maximized")
         self.options.add_argument("disable-infobars")
         self.options.add_argument("--disable-extensions")
         self.options.add_argument("--no-sandbox")
-        self.driver = uc.Chrome(service=ChromeService(ChromeDriverManager().install()), options=self.options)
-        self.driver.maximize_window()
+        self.options.add_argument(" ")
+        
+        if system == "Windows":
+            self.driver = webdriver.Chrome(service=service, options=self.options)
+        elif system == "Linux":
+            self.driver = webdriver.Chrome(service=service, options=self.options)
         self.config = json.load(open('config.json'))
+        self.driver.get('https://br.parimatch.com/')
 
     def login(self):
 
-        self.driver.get('https://br.parimatch.com/')
         sleep(10)
         self.driver.find_element(By.CSS_SELECTOR, 'div.modulor_ripple__wrapper__1_49_1').click()
         sleep(5)
